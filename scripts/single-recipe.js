252ae0c95c874399830b44849490238019773353
Agantuk Sarkar <agantuksarkar124@gmail.com>
2025-02-26 14:09:25 +0530

    update responsiveness

--- scripts/single-recipe.js
+++ scripts/single-recipe.js
@@ -4,11 +4,8 @@ import { displaySingleRecipe } from "../components/display-single-recipe.js";
 // Parse the URL query parameters
 let query_Parameters = new URLSearchParams(window.location.search);
 
-console.log("queryParams:",query_Parameters);
-
 // Get the ID from the query parameters
 let id = query_Parameters.get("id");
-console.log("id:",id);
 
 let recipe_container = document.querySelector(".recipe-card-container");
 
@@ -16,7 +13,6 @@ let singleRecipeArray = [];
 
 async function showSingleRecipeById(id){
     const single_recipe_by_id = await getSingleRecipeById(id);
-    console.log(single_recipe_by_id);
     singleRecipeArray.push(single_recipe_by_id);
 
     displaySingleRecipe(singleRecipeArray,recipe_container);
